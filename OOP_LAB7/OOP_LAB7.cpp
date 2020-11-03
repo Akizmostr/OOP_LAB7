a073164f03de8c6bb2d6a0c4c86567e3435b46d4
Слава Угначёв <nterehina666@mail.ru>
2020-11-03 10:37:06 +0300

    111

--- OOP_LAB7/OOP_LAB7.cpp
+++ OOP_LAB7/OOP_LAB7.cpp
@@ -1,20 +1,45 @@
-﻿// OOP_LAB7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
+class TransException:public std::exception {
+public:
+	TransException(std::string error):m_error(error) {
+	}
+	const char* what() noexcept {
+		return m_error.c_str();
+	}
+private:
+	std::string m_error;
+};
 
-#include <iostream>
+
+class Trans {
+public:
+	double Sin(double);
+	double Cos(double);
+	double Tan(double);
+private:
+
+};
+
+double Trans::Sin(double angle) {
+	double result = sin(angle);
+	if (result > 1 || result < 1) {
+		throw TransException("Sin is going beyond limit values");
+	}
+}
+
+double Trans::Cos(double angle) {
+	double result = Cos(angle);
+	if (result > 1 || result < 1) {
+		throw TransException("Cos is going beyond limit values");
+	}
+}
+
+double Trans::Tan(double angle) {
+
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
